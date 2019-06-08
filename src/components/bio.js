654208c370a1d990bca3974a26df5ec978fdff5e
DanielOhn <ohndaniel@gmail.com>
2019-06-08 12:03:40 -0700

    Add styling.  Update with my profile picture and information.

--- src/components/bio.js
+++ src/components/bio.js
@@ -34,12 +34,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
-              {` `}
-              <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
-              </a>
+              Written by <strong>{author}</strong> who is an upcoming Web Dev.
             </p>
           </Container>
         )
