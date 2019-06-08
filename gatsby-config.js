1aed2f43b3fad3d0f3c68355c56cd5add8c15b6b
DanielOhn <ohndaniel@gmail.com>
2019-06-08 01:16:00 -0700

    Change config settings.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Gatsby Personal Blog`,
+    author: `Daniel Ohn`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://blog-boi.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
